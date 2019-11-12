438b0f28070760076257ca53d5d448013f5ea8fa
Ali Taleb Zadeh <alitk@vt.edu>
2019-11-12 12:02:04 -0500

    Client saves the model after training it

--- client.py
+++ client.py
@@ -12,39 +12,8 @@ from models.Update import LocalUpdate
 from models.Nets import MLP, CNNMnist, CNNCifar
 from models.Fed import FedAvg
 from models.test import test_img
+import os
 
-
-
-
-class LocalUpdate(object):
-    def init(self, args, dataset=None, idxs=None):
-        self.args = args
-        self.loss_func = nn.CrossEntropyLoss()
-        self.selected_clients = []
-		ldr_train = DataLoader(DatasetSplit(dataset, idxs), batch_size=self.args.local_bs, shuffle=True)
-
-    def train(self, net):
-        net.train()
-        # train and update
-        optimizer = torch.optim.SGD(net.parameters(), lr=self.args.lr, momentum=0.5)
-
-        epoch_loss = []
-        for iter in range(self.args.local_ep):
-            batch_loss = []
-            for batch_idx, (images, labels) in enumerate(self.ldr_train):
-                images, labels = images.to(self.args.device), labels.to(self.args.device)
-                net.zero_grad()
-                log_probs = net(images)
-                loss = self.loss_func(log_probs, labels)
-                loss.backward()
-                optimizer.step()
-                if self.args.verbose and batch_idx % 10 == 0:
-                    print('Update Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                        iter, batch_idx * len(images), len(self.ldr_train.dataset),
-                               100. * batch_idx / len(self.ldr_train), loss.item()))
-                batch_loss.append(loss.item())
-            epoch_loss.append(sum(batch_loss)/len(batch_loss))
-        return net.state_dict(), sum(epoch_loss) / len(epoch_loss)
 
 
 
@@ -94,11 +63,13 @@ if __name__ == '__main__':
     net_local=copy.deepcopy(net_glob).to(args.device)
 
 
-    local = LocalUpdate(args=args, dataset=dataset_train, idxs=dict_users[idx])
+    local = LocalUpdate(args=args, dataset=dataset_train, idxs=dict_users[1])
     w, loss = local.train(net=copy.deepcopy(net_glob).to(args.device))
-    
-    w_locals.append(copy.deepcopy(w))
-    loss_locals.append(copy.deepcopy(loss))
+
+    #w_locals.append(copy.deepcopy(w))
+    #loss_locals.append(copy.deepcopy(loss))
+
+    torch.save(w, './LocalModel/local.pth')
 
         
         
