27b9c49d07fd4cb01c534ed5e4a682a19b3da530
Ali Taleb Zadeh <alitk@vt.edu>
2019-11-20 20:55:22 -0500

    added merger server

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -12,38 +17,24 @@ from models.Update import LocalUpdate
 from models.Nets import MLP, CNNMnist, CNNCifar
 from models.Fed import FedAvg
 from models.test import test_img
-import os
-
-
-
 
 
 if __name__ == '__main__':
-    # parse args
     args = args_parser()
     args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
 
-    # load dataset and split users
+
     if args.dataset == 'mnist':
         trans_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-        dataset_train = datasets.MNIST('../data/mnist/', train=True, download=True, transform=trans_mnist)
         dataset_test = datasets.MNIST('../data/mnist/', train=False, download=True, transform=trans_mnist)
         # sample users
-        if args.iid:
-            dict_users = mnist_iid(dataset_train, args.num_users)
-        else:
-            dict_users = mnist_noniid(dataset_train, args.num_users)
     elif args.dataset == 'cifar':
         trans_cifar = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        dataset_train = datasets.CIFAR10('../data/cifar', train=True, download=True, transform=trans_cifar)
         dataset_test = datasets.CIFAR10('../data/cifar', train=False, download=True, transform=trans_cifar)
-        if args.iid:
-            dict_users = cifar_iid(dataset_train, args.num_users)
-        else:
-            exit('Error: only consider IID setting in CIFAR10')
+
     else:
         exit('Error: unrecognized dataset')
-    img_size = dataset_train[0][0].shape
+    img_size = dataset_test[0][0].shape
 
     # build model
     if args.model == 'cnn' and args.dataset == 'cifar':
@@ -59,19 +50,26 @@ if __name__ == '__main__':
         exit('Error: unrecognized model')
     print(net_glob)
     net_glob.train()
+    w_glob = net_glob.state_dict()
 
-    net_local=copy.deepcopy(net_glob).to(args.device)
+    # training
+    
+    w_locals, loss_locals = [], []
+    m = max(int(args.frac * args.num_users), 1)
+    idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+    for idx in range(args.num_users):
+        path='./LocalModel/local{}.pth'.format(idx)
+        w=torch.load(path)
+        w_locals.append(copy.deepcopy(w))
+    # update global weights
+    w_glob = FedAvg(w_locals)
 
+    # copy weight to net_glob
+    net_glob.load_state_dict(w_glob)
 
-    local = LocalUpdate(args=args, dataset=dataset_train, idxs=dict_users[args.idx])
-    w, loss = local.train(net=copy.deepcopy(net_glob).to(args.device))
-
-    #w_locals.append(copy.deepcopy(w))
-    #loss_locals.append(copy.deepcopy(loss))
-    #w['epoch']=0
-
-    torch.save(w, './LocalModel/local{}.pth'.format(args.idx))
-
-        
-        
+    net_glob.eval()
+    #acc_train, loss_train = test_img(net_glob, dataset_train, args)
+    acc_test, loss_test = test_img(net_glob, dataset_test, args)
+    #print("Training accuracy: {:.2f}".format(acc_train))
+    print("Testing accuracy: {:.2f}".format(acc_test))
 
