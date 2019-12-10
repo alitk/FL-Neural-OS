b2bcc176351df8490529665366d88a7a72357bc8
Taleb <alitk@vt.edu>
2019-12-10 10:25:14 -0500

    add a sample test script

--- TestScript.py
+++ TestScript.py
@@ -1,1 +1,7 @@
-os.system("python server.py --num_users 3")
+num_users=10
+import os
+
+for i in range(num_users):
+	os.system("python client.py --new t --idx {} --verbose --num_users {} --iid --local_bs 60".format(i, num_users))
+
+os.system("python server.py --num_users {}".format(num_users))
