e6081c10c7770fff4dfcb6aeca4d2c7a3d2fb77e
Taleb <alitk@vt.edu>
2019-12-09 20:35:34 -0500

    add federated weighted average mode

--- models/Fed.py
+++ models/Fed.py
@@ -7,10 +7,14 @@ import torch
 from torch import nn
 
 
-def FedAvg(w):
+def FedAvg(w, avg_weights):
     w_avg = copy.deepcopy(w[0])
+    sum_avg=sum(avg_weights)
     for k in w_avg.keys():
         for i in range(1, len(w)):
-            w_avg[k] += w[i][k]
-        w_avg[k] = torch.div(w_avg[k], len(w))
+            w_avg[k] += w[i][k]*torch.tensor(avg_weights[i]/sum_avg)
+            
+        #w_avg[k] = torch.div(w_avg[k], sum(avg_weights))
+
+
     return w_avg
--- server.py
+++ server.py
@@ -23,18 +23,27 @@ if __name__ == '__main__':
     args = args_parser()
     args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
 
-
+    # load dataset and split users
     if args.dataset == 'mnist':
         trans_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-        dataset_test = datasets.MNIST('../data/mnist/', train=False, download=True, transform=trans_mnist)
+        dataset_train = datasets.MNIST('./data/mnist/', train=True, download=True, transform=trans_mnist)
+        dataset_test = datasets.MNIST('./data/mnist/', train=False, download=True, transform=trans_mnist)
         # sample users
+        if args.iid:
+            dict_users = mnist_iid(dataset_train, args.num_users, args.seed)
+        else:
+            dict_users = mnist_noniid(dataset_train, args.num_users, args.seed)
     elif args.dataset == 'cifar':
         trans_cifar = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        dataset_test = datasets.CIFAR10('../data/cifar', train=False, download=True, transform=trans_cifar)
-
+        dataset_train = datasets.CIFAR10('./data/cifar', train=True, download=True, transform=trans_cifar)
+        dataset_test = datasets.CIFAR10('./data/cifar', train=False, download=True, transform=trans_cifar)
+        if args.iid:
+            dict_users = cifar_iid(dataset_train, args.num_users, args.seed)
+        else:
+            exit('Error: only consider IID setting in CIFAR10')
     else:
         exit('Error: unrecognized dataset')
-    img_size = dataset_test[0][0].shape
+    img_size = dataset_train[0][0].shape
 
     # build model
     if args.model == 'cnn' and args.dataset == 'cifar':
@@ -54,28 +63,31 @@ if __name__ == '__main__':
 
     # training
     
-    w_locals, loss_locals = [], []
+    w_locals, loss_locals, w_locals_weights = [], [], []
     m = max(int(args.frac * args.num_users), 1)
     idxs_users = np.random.choice(range(args.num_users), m, replace=False)
     for idx in range(args.num_users):
         path=args.local_dir+'local{}.pth'.format(idx)
         checkpoint = torch.load(path)
         w=checkpoint['state_dict']
+        data_weight=checkpoint['data_weight']
         w_locals.append(copy.deepcopy(w))
+        w_locals_weights.append(data_weight)
+
     # update global weights
-    w_glob = FedAvg(w_locals)
+    w_glob = FedAvg(w_locals, w_locals_weights)
 
     # copy weight to net_glob
     net_glob.load_state_dict(w_glob)
 
     net_glob.eval()
-    #acc_train, loss_train = test_img(net_glob, dataset_train, args)
+    acc_train, loss_train = test_img(net_glob, dataset_train, args)
     acc_test, loss_test = test_img(net_glob, dataset_test, args)
-    #print("Training accuracy: {:.2f}".format(acc_train))
+    print("Training accuracy: {:.2f}".format(acc_train))
     print("Testing accuracy: {:.2f}".format(acc_test))
 
 
     torch.save(w_glob, args.saveto+"AvgModel.pth")
-    if rm_local:
+    if args.rm_local:
         empty_folder(args.local_dir)
 
