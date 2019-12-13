5f3174789963ae1eca5d7118cd5c2cbe7122e724
Ali Taleb Zadeh <alitk@vt.edu>
2019-12-13 16:00:11 -0500

    added local_dir

--- StarterScript.py
+++ StarterScript.py
@@ -16,10 +16,10 @@ assert (args.client or args.server), "specify model e.g. --client or --server."
 
 if args.client and not(args.server):
 
-	cmd_run ="python client.py {} --idx {} {} --num_users {} {} --local_bs {} --base_file {}".format(args.new*'--new',
+	cmd_run ="python client.py {} --idx {} {} --num_users {} {} --local_bs {} --base_file {} --local_dir".format(args.new*'--new',
 																										 args.idx, args.verbose*'--verbose',
 																										  args.num_users, args.iid*'--iid',
-																										   args.local_bs, args.base_file)
+																										   args.local_bs, args.base_file, args.local_dir)
 elif not(args.client) and  args.server:
 	cmd_run="python server.py"
 	
